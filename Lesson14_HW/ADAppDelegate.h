6d281416696f25abed7e6aac299aac05b47d3428
xartemx <adtest78@gmail.com>
2014-12-19 19:11:50 -0600

    Initial commit

---
+++ Lesson14_HW/ADAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ADAppDelegate.h
+//  Lesson14_HW
+//
+//  Created by A D on 12/24/13.
+//  Copyright (c) 2013 AD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
